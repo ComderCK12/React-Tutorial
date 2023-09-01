a61ec27347aefe6af88f2c07678ee4b33a950251
ComderCK12 <chiragkathoyeck@gmail.com>
2023-09-01 19:31:42 +0530

    useState - with states

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import "./App.css";
 // import { User } from "./components/User";
 import { useState } from "react";
 
-
 function App() {
 
-  // the initial state (age) set to 0, where age is a variable and setAge is a function
-  const [age, setAge] = useState(0);
+  // useState
+  const [inputValue, setInputValue] = useState("");
 
-  const increaseAge = () => {
-    setAge(age+1);
-  }
-
-  const decreaseAge = () => {
-    setAge(age-1);
-  }
+  // setting the value 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div className="App">
-      <button onClick={decreaseAge}>Decrease</button>
-      {age}
-      <button onClick={increaseAge}>Increase</button>
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
     </div>
   );
 }
-
 
 export default App;
